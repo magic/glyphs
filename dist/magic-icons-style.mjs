3e67a74506d1f1a30a3b0093a35c26f0a480d37a
Jascha <jascha@jaeh.at>
2020-01-06 04:30:54 +0100

    add svgs and dist dir

---
+++ dist/magic-icons-style.mjs
@@ -0,0 +1,38 @@
+{
+  '@font-face': {
+    'fontFamily': 'magic-icons',
+    'fontDir': '/',
+    'fontStyle': 'normal',
+    'fontWeight': 'normal'
+  },
+  '[class^="mi"], [class*="mi"]': {
+    'fontFamily': 'magic-icons',
+    '-webkitFontSmoothing': 'antialiased',
+    '-mozOsxFontSmoothing': 'grayscale',
+    'fontStyle': 'normal'
+  },
+  '.mi-bookmark::before': {
+    'content': '"\ea01"'
+  },
+  '.mi-calendar::before': {
+    'content': '"\ea02"'
+  },
+  '.mi-camera::before': {
+    'content': '"\ea03"'
+  },
+  '.mi-chat::before': {
+    'content': '"\ea04"'
+  },
+  '.mi-check::before': {
+    'content': '"\ea05"'
+  },
+  '.mi-state-beinvited::before': {
+    'content': '"\ea06"'
+  },
+  '.mi-success::before': {
+    'content': '"\ea07"'
+  },
+  '.mi-warning::before': {
+    'content': '"\ea08"'
+  }
+}

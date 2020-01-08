c9859aace12c55e16e9a833af5468562d1972acc
Jascha <jascha@jaeh.at>
2020-01-08 05:03:20 +0100

    only error if none of the passed --dir exist

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,30 @@
+import path from 'path'
+
 import fs from '@magic/fs'
 import log from '@magic/log'
 
 import * as tasks from './tasks/index.mjs'
 
 export const build = async (opts = {}) => {
-  try {
-    const exists = await fs.exists(opts.dir)
-    if (!exists) {
-      throw new Error('ENOENT', `directory ${opts.dir} does not exist`)
-    }
+  let numDirs = opts.dir.length
+    await Promise.all(opts.dir.map(async dir => {
+      if (!path.isAbsolute(dir)) {
+          dir = path.join(process.cwd(), dir)
+      }
 
+      const exists = await fs.exists(dir)
+
+      if (!exists) {
+        numDirs -= 1
+      }
+    }))
+
+  if (numDirs === 0) {
+    log.error(`ENOENT: --dir ${opts.dir.join(' ')} does not exist`)
+    process.exit()
+  }
+
+  try {
     const { glyphs, options } = await tasks.prepare(opts)
 
     const contents = await tasks.build(glyphs, options)

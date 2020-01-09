5aeddf2911f7c2425eed033e3746d2e14dfb9bf6
Jascha <jascha@jaeh.at>
2020-01-09 17:39:40 +0100

    remove unused dependency

--- src/resize.mjs
+++ src/resize.mjs
@@ -4,9 +4,8 @@ import fs from '@magic/fs'
 import is from '@magic/types'
 import log from '@magic/log'
 
-import parse from '@parametric-svg/parse'
-
 export const resize = async (opts = {}) => {
+
 }
 
 export default resize
